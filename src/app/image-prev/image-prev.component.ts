b2cde90d2fa0ada74d347ae657bc7ec31d1643b4
Zachary <heidelzachary@gmail.com>
2021-06-18 18:46:37 -0700

    Update styling and add template image-prev component

---
+++ src/app/image-prev/image-prev.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-prev',
+  templateUrl: './image-prev.component.html',
+  styleUrls: ['./image-prev.component.scss']
+})
+export class ImagePrevComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
