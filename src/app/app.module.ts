f49e9b016bc99e5461502276a9d6f859f0b91be7
Zachary <heidelzachary@gmail.com>
2021-06-22 18:35:50 -0700

    Add Material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ImagePrevComponent } from './image-prev/image-prev.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { ImagePrevComponent } from './image-prev/image-prev.component';
     ImagePrevComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+
+@NgModule({
+  exports: [
+    MatCardModule
+  ]
+})
+export class MaterialModule { }
