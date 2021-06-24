1f0790abfd5ff4d286c2ac2aac1b2ab4719f58a9
Zachary <heidelzachary@gmail.com>
2021-06-23 19:18:34 -0700

    Add card and styling to main page

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   exports: [
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
