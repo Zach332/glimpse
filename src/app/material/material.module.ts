0f743a3d4c49a2e199fe42190e1cf3210d3cfc1d
Zachary Heidel <zachheid@amazon.com>
2021-07-01 18:46:15 -0700

    Add hover style and close button to image-prev component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  exports: [MatCardModule, MatButtonModule, MatSliderModule],
+  exports: [MatCardModule, MatButtonModule, MatSliderModule, MatIconModule],
 })
 export class MaterialModule {}
