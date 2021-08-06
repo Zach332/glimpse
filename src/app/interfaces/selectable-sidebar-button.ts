eccd7758e9a0c9c61c8eefdb24c04dcafe942acc
Zachary Heidel <zachheid@amazon.com>
2021-08-05 19:06:50 -0700

    Make sidebar buttons selectable

---
+++ src/app/interfaces/selectable-sidebar-button.ts
@@ -0,0 +1,4 @@
+import { Selectable } from './selectable';
+import { SidebarButton } from './sidebar-button';
+
+export interface SelectableSidebarButton extends SidebarButton, Selectable {}
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,35 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
+import { Component } from '@angular/core';
 import { SidebarButton } from '../interfaces/sidebar-button';
 import { PageManagerService } from '../page-prev-display/page-manager.service';
+import { SidebarManagerService } from './sidebar-manager.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
-  sidebarButtons: SidebarButton[] = [];
-
-  ngOnInit() {
-    this.sidebarButtons.push({ id: 1, label: 'Saved' });
-    DataService.getAllSavedFolders().then((folders) => {
-      folders.forEach((folder) => {
-        if (folder.folderId !== 1) {
-          this.sidebarButtons.push({ id: folder.folderId, label: folder.name, parent: 1 });
-        }
-      });
-    });
-  }
+export class SidebarComponent {
+  constructor(
+    public pageManagerService: PageManagerService,
+    public sidebarManagerService: SidebarManagerService,
+  ) {}
 
   elementIsCollapsed = (buttonData: SidebarButton): boolean => {
     if (buttonData.parent) {
-      return !this.sidebarButtons.find((button) => button.id === buttonData.parent)?.expanded;
+      return !this.sidebarManagerService.sidebarButtons.collection.find(
+        (button) => button.id === buttonData.parent,
+      )?.expanded;
     }
     return false;
   };
-
-  constructor(public pageManagerService: PageManagerService) {}
 
   selectAll(): void {
     this.pageManagerService.tabElements.selectAll();
