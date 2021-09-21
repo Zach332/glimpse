9881ac9799e34c024adf9de1c93c38bd6eeb4107
Edward Noe <eln4jtt@virginia.edu>
2021-09-20 22:23:45 -0400

    Remove completed TODO

--- src/app/interfaces/page.ts
+++ src/app/interfaces/page.ts
@@ -1,7 +1,6 @@
 import { GlimpseId } from './glimpse-id';
 
 export interface Page {
-  // TODO: Define this long type somewhere so it isn't repeated
   readonly glimpseId: GlimpseId;
   title: string;
   url: string;
