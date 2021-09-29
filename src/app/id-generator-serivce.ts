e5114967f8edb01e3df07bc099cc7a4279bc68bb
Edward Noe <eln4jtt@virginia.edu>
2021-09-28 22:33:20 -0400

    Change IdGeneratorService to use random numbers

--- src/app/id-generator-serivce.ts
+++ src/app/id-generator-serivce.ts
@@ -1,13 +1,9 @@
 import { DataSourceId } from './interfaces/data-source-id';
-import { DataSourceType } from './interfaces/data-source-type';
 import { PageId } from './interfaces/page-id';
 
 export class IdGeneratorService {
-  // TODO: Will all of these even be unique?
+  // TODO: Change this
   public static getIdFromDataSourceIdOrPageId(id: DataSourceId | PageId) {
-    if (id[0] === DataSourceType.Window) {
-      return id[1] * -1;
-    }
-    return parseInt(id[1], 10);
+    return Math.floor(Math.random() * 1000 * 1000 * 1000);
   }
 }
