2546e2d738985212ec305842f3166fe8c09ae050
Edward Noe <eln4jtt@virginia.edu>
2021-11-03 22:32:13 -0400

    Merge branch 'fix-bookmark-favicons'

--- src/app/interfaces/data-source-id.ts
+++ src/app/interfaces/data-source-id.ts
@@ -1,3 +1,8 @@
 import { DataSourceType } from './data-source-type';
 
-export type DataSourceId = [DataSourceType.Window, number] | [DataSourceType.Folder, string];
+export type DataSourceId =
+  // [type, windowId]
+  | [DataSourceType.Window, number]
+  // [type, folderId]
+  // folderId is id of BookmarkTreeNode directly below glimpse root folder
+  | [DataSourceType.Folder, string];
